2b706a99b389a1b3e687eabbe3473bdf74fe800c
Elena Gluzdova <elenagavrilova003@gmail.com>
2023-09-08 15:03:22 +0300

    Update 2023-09-08T12:03:09.383Z

--- app/js/contact.js
+++
@@ -1,8 +0,0 @@
-const blog = document.querySelector('.contacts__blog');
-const btn = document.querySelector('.contacts__blog-closed');
-btn.addEventListener('click', function (e) {
-  e.stopPropagation();
-  e.preventDefault();
-  blog.classList.add('contacts__blog--active');
-
-})
